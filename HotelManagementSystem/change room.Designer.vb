a22f96495baf42c2464482dffb11a537384442f1
Barathwaj05 <barathbalan23@gmail.com>
2025-10-24 10:51:13 +0800

    updated one

--- HotelManagementSystem/change room.Designer.vb
+++ HotelManagementSystem/change room.Designer.vb
@@ -22,86 +22,94 @@ Partial Class change_room
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(change_room))
         Label1 = New Label()
         TextBox1 = New TextBox()
-        Label2 = New Label()
-        Label3 = New Label()
-        TextBox2 = New TextBox()
-        TextBox3 = New TextBox()
+        GroupBox1 = New GroupBox()
+        CurrentBookedRoom = New ListBox()
+        AvailableRoomID = New ListBox()
+        Button1 = New Button()
+        GroupBox1.SuspendLayout()
         SuspendLayout()
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
+        Label1.BackColor = Color.Lavender
         Label1.Font = New Font("Segoe UI", 12F)
-        Label1.Location = New Point(66, 89)
+        Label1.Location = New Point(112, 37)
         Label1.Name = "Label1"
-        Label1.Size = New Size(79, 28)
+        Label1.Size = New Size(88, 28)
         Label1.TabIndex = 0
-        Label1.Text = "Enter IC"
+        Label1.Text = "Enter IC :"
         ' 
         ' TextBox1
         ' 
-        TextBox1.Location = New Point(54, 133)
+        TextBox1.Location = New Point(197, 38)
         TextBox1.Name = "TextBox1"
         TextBox1.Size = New Size(125, 27)
         TextBox1.TabIndex = 1
         ' 
-        ' Label2
+        ' GroupBox1
         ' 
-        Label2.AutoSize = True
-        Label2.Font = New Font("Segoe UI", 12F)
-        Label2.Location = New Point(54, 197)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(158, 28)
-        Label2.TabIndex = 2
-        Label2.Text = "Current Room ID"
+        GroupBox1.BackgroundImage = My.Resources.Resources.Blur_Gradient_Vectors___Download_Free_High_Quality_Vectors_from_Freepik___Freepik
+        GroupBox1.Controls.Add(Button1)
+        GroupBox1.Controls.Add(AvailableRoomID)
+        GroupBox1.Controls.Add(CurrentBookedRoom)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        GroupBox1.Location = New Point(140, 27)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(491, 397)
+        GroupBox1.TabIndex = 6
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Change Room"
         ' 
-        ' Label3
+        ' CurrentBookedRoom
         ' 
-        Label3.AutoSize = True
-        Label3.Font = New Font("Segoe UI", 12F)
-        Label3.Location = New Point(54, 294)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(130, 28)
-        Label3.TabIndex = 3
-        Label3.Text = "New Room Id"
+        CurrentBookedRoom.FormattingEnabled = True
+        CurrentBookedRoom.Location = New Point(25, 114)
+        CurrentBookedRoom.Name = "CurrentBookedRoom"
+        CurrentBookedRoom.Size = New Size(209, 224)
+        CurrentBookedRoom.TabIndex = 2
         ' 
-        ' TextBox2
+        ' AvailableRoomID
         ' 
-        TextBox2.Location = New Point(54, 334)
-        TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(125, 27)
-        TextBox2.TabIndex = 4
+        AvailableRoomID.FormattingEnabled = True
+        AvailableRoomID.Location = New Point(263, 115)
+        AvailableRoomID.Name = "AvailableRoomID"
+        AvailableRoomID.Size = New Size(209, 224)
+        AvailableRoomID.TabIndex = 3
         ' 
-        ' TextBox3
+        ' Button1
         ' 
-        TextBox3.Location = New Point(54, 242)
-        TextBox3.Name = "TextBox3"
-        TextBox3.Size = New Size(125, 27)
-        TextBox3.TabIndex = 5
+        Button1.BackColor = Color.Lavender
+        Button1.Location = New Point(197, 345)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 4
+        Button1.Text = "Proceed"
+        Button1.UseVisualStyleBackColor = False
         ' 
         ' change_room
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
         ClientSize = New Size(800, 450)
-        Controls.Add(TextBox3)
-        Controls.Add(TextBox2)
-        Controls.Add(Label3)
-        Controls.Add(Label2)
-        Controls.Add(TextBox1)
-        Controls.Add(Label1)
+        Controls.Add(GroupBox1)
         Name = "change_room"
         Text = "change_room"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
         ResumeLayout(False)
-        PerformLayout()
     End Sub
 
     Friend WithEvents Label1 As Label
     Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents CurrentBookedRoom As ListBox
+    Friend WithEvents AvailableRoomID As ListBox
+    Friend WithEvents Button1 As Button
 End Class
