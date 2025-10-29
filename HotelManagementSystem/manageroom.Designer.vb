ed78d467ce201eac53300019fbbe9db806b4ade5
Kabhi04 <kabilan163669@gmail.com>
2025-10-29 22:50:02 +0800

    form2 baru manageroom

---
+++ HotelManagementSystem/manageroom.Designer.vb
@@ -0,0 +1,269 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class manageroom
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(manageroom))
+        GroupBox1 = New GroupBox()
+        ListBox2 = New ListBox()
+        TextBox4 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox2 = New TextBox()
+        ListBox1 = New ListBox()
+        Button1 = New Button()
+        Label7 = New Label()
+        Label6 = New Label()
+        Label5 = New Label()
+        Label4 = New Label()
+        Label3 = New Label()
+        Label2 = New Label()
+        GroupBox2 = New GroupBox()
+        TextBox1 = New TextBox()
+        Button2 = New Button()
+        Label8 = New Label()
+        Label1 = New Label()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.BackColor = Color.Transparent
+        GroupBox1.Controls.Add(ListBox2)
+        GroupBox1.Controls.Add(TextBox4)
+        GroupBox1.Controls.Add(TextBox3)
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Controls.Add(ListBox1)
+        GroupBox1.Controls.Add(Button1)
+        GroupBox1.Controls.Add(Label7)
+        GroupBox1.Controls.Add(Label6)
+        GroupBox1.Controls.Add(Label5)
+        GroupBox1.Controls.Add(Label4)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        GroupBox1.Location = New Point(57, 83)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(424, 461)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Add Room"
+        ' 
+        ' ListBox2
+        ' 
+        ListBox2.FormattingEnabled = True
+        ListBox2.Items.AddRange(New Object() {"Available", "Reserved", "Unavailable"})
+        ListBox2.Location = New Point(152, 298)
+        ListBox2.Name = "ListBox2"
+        ListBox2.Size = New Size(249, 64)
+        ListBox2.TabIndex = 12
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(150, 245)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(249, 27)
+        TextBox4.TabIndex = 11
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(150, 181)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(249, 27)
+        TextBox3.TabIndex = 10
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(150, 138)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(249, 27)
+        TextBox2.TabIndex = 9
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.Items.AddRange(New Object() {"Single", "Twin", "Deluxe", "Standard"})
+        ListBox1.Location = New Point(150, 53)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(249, 64)
+        ListBox1.TabIndex = 7
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(305, 405)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 6
+        Button1.Text = "Add"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label7.Location = New Point(14, 298)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(132, 28)
+        Label7.TabIndex = 5
+        Label7.Text = "Room Status"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label6.Location = New Point(14, 245)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(130, 28)
+        Label6.TabIndex = 4
+        Label6.Text = "Room  Rules"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label5.Location = New Point(24, 177)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(91, 28)
+        Label5.TabIndex = 3
+        Label5.Text = "Room Id"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(22, 134)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(120, 28)
+        Label4.TabIndex = 2
+        Label4.Text = "Room Price"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(24, 53)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(118, 28)
+        Label3.TabIndex = 1
+        Label3.Text = "Room Type"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(-51, 53)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(55, 20)
+        Label2.TabIndex = 0
+        Label2.Text = "Label2"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.BackColor = Color.Transparent
+        GroupBox2.Controls.Add(TextBox1)
+        GroupBox2.Controls.Add(Button2)
+        GroupBox2.Controls.Add(Label8)
+        GroupBox2.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        GroupBox2.Location = New Point(546, 83)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(390, 461)
+        GroupBox2.TabIndex = 1
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Delete Room"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(134, 65)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(238, 27)
+        TextBox1.TabIndex = 8
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(278, 405)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 7
+        Button2.Text = "Delete"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label8.Location = New Point(20, 65)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(94, 28)
+        Label8.TabIndex = 2
+        Label8.Text = "Room ID"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Times New Roman", 24F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = SystemColors.HotTrack
+        Label1.Location = New Point(373, 22)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(284, 45)
+        Label1.TabIndex = 2
+        Label1.Text = "Manage Rooms"
+        ' 
+        ' manageroom
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(1053, 642)
+        Controls.Add(Label1)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        ForeColor = SystemColors.ControlText
+        Name = "manageroom"
+        Text = "manageroom"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+End Class
---
+++ HotelManagementSystem/manageroom.vb
@@ -0,0 +1,74 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class manageroom
+    Private connString As String = "server=localhost;userid=root;password=;database=hoteldb"
+
+    ' Populate room type list once
+    Private Sub manageroom_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Items.Clear()
+        ListBox1.Items.Add("Single")
+        ListBox1.Items.Add("Twin")
+        ListBox1.Items.Add("Standard")
+        ListBox1.Items.Add("Deluxe")
+
+        ListBox2.Items.Clear()
+        ListBox2.Items.Add("Available")
+        ListBox2.Items.Add("Unavailable")
+        ListBox2.Items.Add("Reserved")
+
+        ListBox1.SelectionMode = SelectionMode.One
+        ListBox2.SelectionMode = SelectionMode.One
+    End Sub
+
+    ' When staff enters room ID, load room info
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If TextBox1.Text = "" Then Exit Sub
+        Try
+            Using conn As New MySqlConnection(connString)
+                conn.Open()
+                Dim cmd As New MySqlCommand("SELECT * FROM rooms WHERE room_id=@id", conn)
+                cmd.Parameters.AddWithValue("@id", TextBox1.Text)
+                Dim reader As MySqlDataReader = cmd.ExecuteReader()
+                If reader.Read() Then
+                    TextBox2.Text = reader("price").ToString()
+                    TextBox3.Text = reader("room_id").ToString()
+                    TextBox4.Text = reader("room_rules").ToString()
+                    ' Select type in ListBox1
+                    ListBox1.SelectedItem = reader("room_type").ToString()
+                    ' Select status in ListBox2
+                    ListBox2.SelectedItem = reader("roomstatus").ToString()
+                End If
+            End Using
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    ' Update room in database
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Or ListBox1.SelectedIndex = -1 Or ListBox2.SelectedIndex = -1 Then
+            MessageBox.Show("Please fill all fields and select type/status.")
+            Exit Sub
+        End If
+
+        Try
+            Using conn As New MySqlConnection(connString)
+                conn.Open()
+                Dim cmd As New MySqlCommand("UPDATE rooms SET room_type=@type, price=@price, room_rules=@rules, roomstatus=@status WHERE room_id=@id", conn)
+                cmd.Parameters.AddWithValue("@id", TextBox1.Text)
+                cmd.Parameters.AddWithValue("@type", ListBox1.SelectedItem.ToString())
+                cmd.Parameters.AddWithValue("@price", CDec(TextBox2.Text))
+                cmd.Parameters.AddWithValue("@rules", TextBox4.Text)
+                cmd.Parameters.AddWithValue("@status", ListBox2.SelectedItem.ToString())
+                Dim rowsAffected As Integer = cmd.ExecuteNonQuery()
+                If rowsAffected > 0 Then
+                    MessageBox.Show("Room updated successfully.")
+                Else
+                    MessageBox.Show("Room ID not found.")
+                End If
+            End Using
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Class
